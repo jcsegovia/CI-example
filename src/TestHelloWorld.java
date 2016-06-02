b715ec31b2321856fe2b90ffb3a73979a42824a2
Juan Carlos <juancarlos.segovia@gmail.com>
2016-06-02 12:03:58 +0200

    New test name

--- src/TestHelloWorld.java
+++ src/TestHelloWorld.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 public class TestHelloWorld {
 
 	@org.junit.Test
-	public void test1(){
+	public void testHelloWorld(){
 		HelloWorld hw = new HelloWorld();
 		hw.setMsg("Hola");
 		Assert.assertEquals("Hola", hw.getMsg());
